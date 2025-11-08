905f8bd4de3f4d8d13aeede253c0fd79eee55b41
romaTk <rt.craz@gmail.com>
2025-11-08 03:26:01 +0000

    Add get-replace-with.sh script and update build args in envs.json

---
+++ env-scripts/docker/dood/volume-path-replacer/get-replace-with.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function main {
+    
+    if [[ -f "/working-env/docker/dood/volume-path-replacer/config.cfg" ]]; then
+        source /working-env/docker/dood/volume-path-replacer/config.cfg
+        if [ $? -ne 0 ]; then
+            echo "Error sourcing config file" >&2
+            exit 1
+        fi
+        
+        echo "$REPLACE_WITH"
+    else
+        echo "${pwd}"
+    fi
+
+    exit 0
+}
